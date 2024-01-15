e2b92185f03f57356989bc8ac9aba4ce9c715256
dhanashree-11 <dhanashree1103@gmail.com>
2024-01-15 20:02:38 +0530

    first commit

---
+++ 3d_collab/app/src/main/java/com/example/a3d_collab/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.a3d_collab
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.widget.SwitchCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val switch = findViewById<SwitchCompat>(R.id.switch1)
+
+        switch.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (!isChecked) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            }
+            delegate.applyDayNight()
+        }
+    }
+}
